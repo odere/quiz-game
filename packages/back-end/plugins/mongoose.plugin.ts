440dd560f528077d9c9ef50894c6babe0d508ed0
oderechei <aleksandr.derechei#@gmail.com>
2021-07-11 23:05:58 +0300

    QG-0016 - Create the server with logging and DB connection

---
+++ packages/back-end/plugins/mongoose.plugin.ts
@@ -0,0 +1,22 @@
+import Hapi from '@hapi/hapi'
+import mongoose from "mongoose"
+
+import '../models/question.model'
+
+const mongoosePlugin = {
+    name: 'app/mongoosePlugin',
+    register: async (server: Hapi.Server) => {
+        try {
+            await mongoose.connect(process.env.MONGO_URI || '', {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+                useFindAndModify: false,
+                useCreateIndex: true,
+            });
+        } catch (error) {
+            console.error(error);
+        }
+    },
+}
+
+export default mongoosePlugin
---
+++ packages/back-end/server.ts
@@ -0,0 +1,41 @@
+import Hapi from "@hapi/hapi"
+import dotenv from "dotenv";
+import path from 'path'
+
+import loggerPlugin from "./plugins/logger.plugin"
+import mongoosePlugin from "./plugins/mongoose.plugin"
+import isProduction from "./utils/isProduction"
+
+if (!isProduction) {
+    dotenv.config({ path: path.resolve(__dirname, './.env') })
+}
+
+const server: Hapi.Server = Hapi.server({
+    port: process.env.BACKEND_PORT || 5000,
+    host: process.env.HOST || "0.0.0.0",
+});
+
+export const createServer = async (): Promise<Hapi.Server> => {
+    // Register middleware
+    await server.register([
+        loggerPlugin,
+        mongoosePlugin,
+    ])
+
+    await server.initialize();
+
+    return server;
+}
+
+export const startServer = async (server: Hapi.Server): Promise<Hapi.Server> => {
+    await server.start();
+
+    server.log("info", `Server running on ${server.info.uri}`);
+
+    return server;
+}
+
+process.on("unhandledRejection", (err) => {
+    console.log(err);
+    process.exit(1);
+});
